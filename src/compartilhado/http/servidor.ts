25bc58509405ff8c10012eac402ff02620530015
Graciliano-Barroso <grico484@gmail.com>
2023-03-08 12:03:23 -0400

    Configuração com a conexão do banco de dados

--- src/compartilhado/http/servidor.ts
+++ src/compartilhado/http/servidor.ts
@@ -1,35 +1,10 @@
 import 'dotenv/config'
-import express, { NextFunction, Request, Response } from 'express'
-import 'express-async-errors'
-import swaggerUi from 'swagger-ui-express'
-import cors from 'cors'
-import { rotas } from './rotas/indice'
-import { ErroDeAplicativo } from '@compartilhado/erros/ErroDeAplicativo'
-import swaggerFile from '../../swagger.json'
+import 'reflect-metadata'
+import { app } from './app'
+import { dataSource } from '@compartilhado/typeorm/indice'
 
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
-
-app.use(rotas)
-
-app.use(
-  (erro: Error, request: Request, response: Response, next: NextFunction) => {
-    if (erro instanceof ErroDeAplicativo) {
-      return response.status(erro.codigoDeStatus).json({
-        estado: 'erro',
-        mensagem: erro.mensagem,
-      })
-    }
-    console.log(erro)
-    return response.status(500).json({
-      estado: 'erro',
-      mensagem: 'Erro interno do servidor',
-    })
-  },
-)
-
-app.listen(process.env.PORT, () => {
-  console.log(`Servidor iniciado na porta ${process.env.PORT}! 🏆`)
+dataSource.initialize().then(() => {
+    app.listen(process.env.PORT, () => {
+    console.log(`Servidor iniciado na porta ${process.env.PORT}! 🏆`)
+  })
 })
