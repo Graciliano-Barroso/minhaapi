c8574cc833a79e17fd80de4d590c45d34b6aa016
Graciliano-Barroso <grico484@gmail.com>
2023-03-09 14:36:34 -0400

    Refatorar entidade papel

--- src/compartilhado/typeorm/indice.ts
+++ src/compartilhado/typeorm/indice.ts
@@ -1,9 +1,10 @@
+import { Papel } from '@papeis/entidades/Papel'
 import { DataSource } from 'typeorm'
 import { CriarTabelaDePapeis1678368552706 } from './migracoes/1678368552706-CriarTabelaDePapeis'
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
-  entities: [],
+  entities: [Papel],
   migrations: [CriarTabelaDePapeis1678368552706],
 })
--- src/papeis/entidades/Papel.ts
+++ src/papeis/entidades/Papel.ts
@@ -1,9 +1,18 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm'
 import { v4 as uuidv4 } from 'uuid'
 
+@Entity('roles')
 export class Papel {
+  @PrimaryColumn()
   id?: string
+
+  @Column()
   nome: string
+
+  @Column()
   idade: number
+
+  @CreateDateColumn()
   criado_em: Date
 
   constructor() {
