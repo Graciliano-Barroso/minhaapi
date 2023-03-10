2f24823cf9c5c09986e99c056ee2e8f9de61e6b5
Graciliano-Barroso <grico484@gmail.com>
2023-03-10 11:39:58 -0400

    Refatorado repositório de papeis

--- src/papeis/repositorios/RepositorioDePapeis.ts
+++ src/papeis/repositorios/RepositorioDePapeis.ts
@@ -1,15 +1,31 @@
+import { dataSource } from '@compartilhado/typeorm/indice'
 import { Papel } from '@papeis/entidades/Papel'
+import { Repository } from 'typeorm'
 
 type CriarPapelDTO = {
   nome: string
   idade: number
 }
+
+export type PaginarParametros = {
+  pagina: number
+  pular: number
+  pegar: number
+}
+
+export type PropriedadesDePaginacaoDePapeis = {
+  por_pagina: number
+  total: number
+  pagina_atual: number
+  dados: Papel[]
+}
+
 export class RepositorioDePapeis {
-  private papeis: Papel[]
+  private repositorio: Repository<Papel>
   private static INSTANCIA: RepositorioDePapeis
 
   private constructor() {
-    this.papeis = []
+    this.repositorio = dataSource.getRepository(Papel)
   }
 
   public static obterInstancia(): RepositorioDePapeis {
@@ -18,24 +34,43 @@ export class RepositorioDePapeis {
     }
     return RepositorioDePapeis.INSTANCIA
   }
-  criar({ nome, idade }: CriarPapelDTO): Papel {
-    const papel = new Papel()
-
-    Object.assign(papel, {
-      nome,
-      idade,
-      criado_em: new Date(),
-    })
-
-    this.papeis.push(papel)
-    return papel
+  async criar({ nome, idade }: CriarPapelDTO): Promise<Papel> {
+    const papel = this.repositorio.create({ nome, idade })
+    return this.repositorio.save(papel)
   }
 
-  encontrarTudo(): Papel[] {
-    return this.papeis
+  async salvar(papel: Papel): Promise<Papel> {
+    return this.repositorio.save(papel)
   }
 
-  encontrarPeloNome(nome: string): Papel | undefined {
-    return this.papeis.find(papel => papel.nome === nome)
+  async excluir(papel: Papel): Promise<void> {
+    await this.repositorio.remove(papel)
+  }
+
+  async encontrarTudo({
+    pagina,
+    pular,
+    pegar,
+  }: PaginarParametros): Promise<PropriedadesDePaginacaoDePapeis> {
+    const [papeis, contar] = await this.repositorio
+      .createQueryBuilder()
+      .skip(pular)
+      .take(pegar)
+      .getManyAndCount()
+    const resultado = {
+      por_pagina: pegar,
+      total: contar,
+      pagina_atual: pagina,
+      dados: papeis,
+    }
+    return resultado
+  }
+
+  async encontrarPeloNome(nome: string): Promise<Papel | null> {
+    return this.repositorio.findOneBy({ nome })
+  }
+
+  async encontrarPeloId(id: string): Promise<Papel | null> {
+    return this.repositorio.findOneBy({ id })
   }
 }
