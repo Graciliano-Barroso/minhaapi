d76a946b1ee9f56fb62556f6cb2566146cb2c022
Graciliano-Barroso <grico484@gmail.com>
2023-03-22 06:25:44 -0400

    Criar interface do repositório de usuário

---
+++ src/usuarios/repositorios/IRepositorioDeUsuario.ts
@@ -0,0 +1,37 @@
+import { Papel } from '@papeis/entidades/Papel'
+import { Usuario } from '@usuarios/entidades/Usuario'
+
+export type CriarUsuarioDTO = {
+  nome: string
+  idade: number
+  email: string
+  senha: string
+  isAdmin: boolean
+  papel: Papel
+}
+
+export type PaginarParametros = {
+  pagina: number
+  pular: number
+  pegar: number
+}
+
+export type PropriedadesDePaginacaoDeUsuario = {
+  por_pagina: number
+  total: number
+  pagina_atual: number
+  dados: Usuario[]
+}
+
+export interface IRepositorioDeUsuarios {
+  criar({ nome, idade, email, senha, isAdmin, papel }: CriarUsuarioDTO): Promise<Usuario>
+  salvar(usuario: Usuario): Promise<Usuario>
+  encontrarTudo({
+    pagina,
+    pular,
+    pegar,
+  }: PaginarParametros): Promise<PropriedadesDePaginacaoDeUsuario>
+  encontrarPeloId(id: string): Promise<Usuario | null>
+  encontrarPeloNome(nome: string): Promise<Usuario | null>
+  excluir(usuario: Usuario): Promise<void>
+}
