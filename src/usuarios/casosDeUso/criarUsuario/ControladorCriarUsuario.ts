28239e4bb471874c4d5c1f515057e4f15aa50f11
Graciliano-Barroso <grico484@gmail.com>
2023-03-28 09:33:31 -0400

    Adicionado ControladorCriarUsuario

---
+++ src/usuarios/casosDeUso/criarUsuario/ControladorCriarUsuario.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express'
+import { container } from 'tsyringe'
+import { CasoDeUsoCriarUsuario } from './CasoDeUsoCriarUsuario'
+
+export class ControladorCriarUsuario {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const casoDeUsoCriarUsuario = container.resolve(CasoDeUsoCriarUsuario)
+    const { nome, idade, email, senha, isAdmin, papelId } = request.body
+    const usuario = await casoDeUsoCriarUsuario.execute({
+      nome,
+      idade,
+      email,
+      senha,
+      isAdmin,
+      papelId,
+    })
+    return response.status(201).json(usuario)
+  }
+}
