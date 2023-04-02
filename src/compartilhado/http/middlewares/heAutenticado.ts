00423985eae8d36cb7945f112eed2a1382b1f20c
Graciliano-Barroso <grico484@gmail.com>
2023-04-02 18:08:24 -0400

    Refatorado middleware heAutenticado

--- src/compartilhado/http/middlewares/heAutenticado.ts
+++ src/compartilhado/http/middlewares/heAutenticado.ts
@@ -3,19 +3,25 @@ import { NextFunction, Request, Response } from 'express'
 import { Secret, verify } from 'jsonwebtoken'
 import autencticacaoConfig from '@config/autenticacao'
 
+type JwtPayloadProps = {
+  sub: string
+}
+
 export const heAutenticado = (
-  solicitar: Request,
-  resposta: Response,
-  proximo: NextFunction,
+  request: Request,
+  response: Response,
+  next: NextFunction,
 ) => {
-  const cabecalhoAutenticacao = solicitar.headers.authorization
+  const cabecalhoAutenticacao = request.headers.authorization
   if (!cabecalhoAutenticacao) {
     throw new ErroDeAplicativo('Falha ao ferificar o token de acesso', 401)
   }
   const token = cabecalhoAutenticacao.replace('Bearer ', '')
   try {
-    verify(token, autencticacaoConfig.jwt.secret as Secret)
-    return proximo()
+    const tokenDecodificado = verify(token, autencticacaoConfig.jwt.secret as Secret)
+    const { sub } = tokenDecodificado as JwtPayloadProps
+    request.usuario = { id: sub }
+    return next()
   } catch {
     throw new ErroDeAplicativo('Token de autenticação inválido', 401)
   }
